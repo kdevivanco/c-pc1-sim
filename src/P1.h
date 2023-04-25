abd89866b63432eaedd6a11a4ed0e76a40ae7d84
kdevivanco <kdevivanco@gmail.com>
2023-04-25 10:22:44 -0500

    terminando p1, pasa un test

--- src/P1.h
+++ src/P1.h
@@ -22,25 +22,45 @@ class symmetric_difference_t {
 private:
     vector<T> res = {};
 //    T* data_ = nullptr;
-    size_t size_ = 0;
+    //size_t size_ = 0;
 public:
     symmetric_difference_t<T>(const std::vector<T>& v1,const std::vector<T>& v2) {
         // UNIR VECTORES
         //vector<T> merged = v1;
-        std::vector<int> result;
+        vector<T> merged;
         for (int i = 0; i < v1.size(); i++) {
             bool found = false;
             for (int j = 0; j < v2.size(); j++) {
                 if (v1[i] == v2[j]) {
                     found = true;
+                    break;
                 }
             }
             if (!found) {
-                result.push_back(v1[i]);
+                merged.push_back(v1[i]);
             }
         }
-        res = result;
+        for (int i = 0; i < v2.size(); i++) {
+            bool found = false;
+            for (int j = 0; j < v1.size(); j++) {
+                if (v2[i] == v1[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                merged.push_back(v2[i]);
+            }
+        }
+        merged;
+        sort(merged.begin(), merged.end());
+        //unique(merged.begin(),merged.end());
+        res = merged;
+        auto last = std::unique(res.begin(), res.end()); // Borra duplicados
+        res.erase(last, res.end());
+
     }
+
 //        merged.insert(merged.end(), v2.begin(), v2.end());
 //        sort(merged.begin(), merged.end());
 //
@@ -59,10 +79,17 @@ public:
 //        data_ = new T[merged.size()];
 //        std::copy(merged.begin(), merged.end(), data_);
         //res = merged;
-    }
+    //}
 
 //    ~symmetric_difference_t() {
 //        delete[] res;
+////    }
+//    vector<T> removeDuplicates()
+//    {
+//        std::sort(res.begin(), res.end());
+//        auto last = std::unique(res.begin(), res.end());
+//        res.erase(last, res.end());
+//        return res;
 //    }
 
     size_t size()  {
