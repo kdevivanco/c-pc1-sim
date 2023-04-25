ad24171261c9c2e37635fd07010f8163f9711b35
kdevivanco <kdevivanco@gmail.com>
2023-04-25 09:53:50 -0500

    dos soluciones para p1

--- src/P1.cpp
+++ src/P1.cpp
@@ -4,128 +4,26 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <string>
 #include "P1.h"
 
-using namespace std;
-template <typename T>
-class symmetric_difference_t {
-public:
-    symmetric_difference_t(const std::vector<T>& v1, const std::vector<T>& v2) {
-        // unimos los vectores y los ordenamos
-        std::vector<T> merged = v1;
-        merged.insert(merged.end(), v2.begin(), v2.end());
-        std::sort(merged.begin(), merged.end());
+void question_1(){
 
-        // encontramos los elementos que están solamente en uno de los vectores
-        for (auto it = merged.begin(); it != merged.end();) {
-            auto next_it = std::next(it);
-            if (next_it != merged.end() && *it == *next_it) {
-                // el elemento está en ambos vectores, lo eliminamos
-                it = merged.erase(it, next_it + 1);
-            } else {
-                ++it;
-            }
-        }
-
-        // asignamos el resultado a nuestro arreglo dinámico
-        data_ = new T[merged.size()];
-        std::copy(merged.begin(), merged.end(), data_);
-        size_ = merged.size();
-    }
-
-    ~symmetric_difference_t() {
-        delete[] data_;
-    }
-
-    // método que retorna el tamaño del arreglo dinámico
-    size_t size() const {
-        return size_;
-    }
-
-    // sobrecarga del operador de paréntesis para acceder al arreglo
-    T* operator()() const {
-        return data_;
-    }
-
-    // sobrecarga del operador de negación para invertir el orden del arreglo
-    void operator!() {
-        std::reverse(data_, data_ + size_);
-    }
-
-private:
-    T* data_ = nullptr; // puntero al arreglo dinámico
-    size_t size_ = 0; // tamaño del arreglo dinámico
-};
-//template<typename T>
-//class symmetric_difference_t {
-//public:
-//    symmetric_difference_t(const std::vector<T>& v1, const std::vector<T>& v2) {
-//        std::vector<T> tmp1(v1);
-//        std::vector<T> tmp2(v2);
-//        std::sort(tmp1.begin(), tmp1.end());
-//        std::sort(tmp2.begin(), tmp2.end());
-//        std::vector<T> result;
-//        std::set_symmetric_difference(tmp1.begin(), tmp1.end(), tmp2.begin(), tmp2.end(), std::back_inserter(result));
-//        data = std::make_shared<std::vector<T>>(result);
-//    }
-//
-//    size_t size() const {
-//        return data->size();
-//    }
-//
-//    T* operator()() const {
-//        return data->data();
-//    }
-//
-//    T* operator!() const {
-//        std::reverse(data->begin(), data->end());
-//        return data->data();
-//    }
-//
-//private:
-//    std::shared_ptr<std::vector<T>> data;
-//};
-//template <class T>
-//class symmetric_difference_t{
-//    typedef T value_type;
-//    typedef typename std::vector<T>::size_type size_type;
-//    //Constructores
-//    symmetric_difference_t();
-//    symmetric_difference_t(initializer_list<T> il);
-//
-//    //Contador de elementos en sym...
-//    size_type size() const {return data->size();}
-//    bool empty() const {return data->empty();}
-//
-//    //Anadir/Remover elementos
-//    void push_back(const T &t){data ->push_back(t);}
-//    void push_back(T &&t){data ->push_back(move(t));} //mover
-//    void pop_back();
-//
-//    //Acceso a elementos:
-//    T& back();
-//    T& operator[](size_type i);
-//private:
-//    shared_ptr< vector<T> > data;
-//
-//    void check(size_type i , const string &msg) const;
-//};
-//
-//template <typename T>
-//symmetric_difference_t<T>::symmetric_difference_t(): data(std::make_shared<std::vector<T>> ()) { };
-//
-//template <typename T>
-//symmetric_difference_t<T>::symmetric_difference_t(std::initializer_list<T> il):
-//        data(std::make_shared<std::vector<T>> (il)) { }
-//
-//
-//
-//void question_1(){
-//
-//}
-
-int main() {
-    symmetric_difference_t<int> sd1 ({1, 5, 3, 8, 2}, {4, 9, 7, 6, 10, 1});
-    return 0;
 }
+
+//int main() {
+//    // genera un arreglo dinámico
+//    symmetric_difference_t<int> sd1 ({1, 5, 3, 8, 2}, {4, 9, 7, 6, 10, 1});
+//// imprimiendo los datos
+//    std::copy(sd1(), sd1() + sd1.size(), std::ostream_iterator<int>(std::cout," "));
+//    std::cout << std::endl;
+//// 2 3 4 5 6 7 8 9 10
+//    !sd1;
+//    std::copy(sd1(), sd1()+sd1.size(),
+//              std::ostream_iterator<int>(std::cout," ");
+//    std::cout << std::endl;
+//// 10 9 8 7 6 5 4 3 2
+//
+//    return 0;
+//}
--- src/P1.h
+++ src/P1.h
@@ -6,6 +6,83 @@
 #define prog3_simulacro_PC1_v2023_1_P1_H
 
 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include "P1.h"
+
+using namespace std;
+template <typename T>
+class symmetric_difference_t {
+private:
+    vector<T> res = {};
+//    T* data_ = nullptr;
+    size_t size_ = 0;
+public:
+    symmetric_difference_t<T>(const std::vector<T>& v1,const std::vector<T>& v2) {
+        // UNIR VECTORES
+        //vector<T> merged = v1;
+        std::vector<int> result;
+        for (int i = 0; i < v1.size(); i++) {
+            bool found = false;
+            for (int j = 0; j < v2.size(); j++) {
+                if (v1[i] == v2[j]) {
+                    found = true;
+                }
+            }
+            if (!found) {
+                result.push_back(v1[i]);
+            }
+        }
+        res = result;
+    }
+//        merged.insert(merged.end(), v2.begin(), v2.end());
+//        sort(merged.begin(), merged.end());
+//
+//        //Eliminar valores iguales
+//        for (auto it = merged.begin(); it != merged.end();) {
+//            auto next_it = std::next(it);
+//            if (next_it != merged.end() && *it == *next_it) {
+//                it = merged.erase(it, next_it + 1);
+//            } else {
+//                ++it;
+//            }
+//        }
+
+
+//        // RESULTADO
+//        data_ = new T[merged.size()];
+//        std::copy(merged.begin(), merged.end(), data_);
+        //res = merged;
+    }
+
+//    ~symmetric_difference_t() {
+//        delete[] res;
+//    }
+
+    size_t size()  {
+        return res.size();
+    }
+
+    T* operator()()  {
+        return res.data();
+    }
+
+    T* operator!() {
+        std::reverse(res.begin(), res.end());
+        return res.data();
+    }
+
+
+};
+
+
+
 void question_1();
 
 #endif //prog3_simulacro_PC1_v2023_1_P1_H
