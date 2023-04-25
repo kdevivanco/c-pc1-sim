d5e79b2fd3e19908ae5a8349c91b9aecb1f832b5
kdevivanco <kdevivanco@gmail.com>
2023-04-25 11:40:49 -0500

    trabajndo p2

--- src/P2.cpp
+++ src/P2.cpp
@@ -3,48 +3,49 @@
 //
 
 #include <iostream>
-#include<vector>
+#include <vector>
 #include <deque>
 #include <string>
 #include <list>
+#include <array>
 #include "P2.h"
 
 using namespace std;
 
-template<typename T, typename res_cont>
-res_cont get_maximum_product_range(T cnt){
-    for (auto iter = cnt.cbegin(); iter < cnt.cend(); /*iter++*/)
-    {
-        if (*iter == 0)
-        {
-            iter = cnt.erase(iter);
-        }
-        else
-        {
-            ++iter;
-        }
-    }
-    return cnt;
 
-}
+
+
+
+//template<typename T, typename res_cont>
+//res_cont get_maximum_product_range(T cnt){
+//    for (auto iter = cnt.cbegin(); iter < cnt.cend(); /*iter++*/)
+//    {
+//        if (*iter == 0)
+//        {
+//            iter = cnt.erase(iter);
+//        }
+//        else
+//        {
+//            ++iter;
+//        }
+//    }
+//    return cnt;
+//
+//}
 void question_2(){
-
-}
-
-
-
-int main(){
     std::list<int> lst1 = {3, 3, -3, 5, 4, -4, 0, -4, -4, -5};
     std::deque f1 = get_maximum_product_range<std::deque>(lst1);
     for (const auto& row: f1) {
-        for(const auto& value: row)
-            std::cout << value << " ";
-        std::cout << std::endl;
-    }
+       for(const auto& value: row)
+           std::cout << value << " ";
+       std::cout << std::endl;
+   }
 
-/*
- Se imprimirá:
-    3 3 5 4 -4 -4 -4 -5
-*/
+}
 
+//
+//
+int main(){
+    question_2();
+//
 }
--- src/P2.h
+++ src/P2.h
@@ -1,10 +1,60 @@
 //
 // Created by rudri on 10/11/2020.
 //
-
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <deque>
+#include <list>
 #ifndef prog3_simulacro_PC1_v2023_1_P2_H
 #define prog3_simulacro_PC1_v2023_1_P2_H
 
+//template<typename Container, typename Container::value_type, typename T>
+//Container get_maximum_product_range(const T& cnt)
+//{
+//    typename Container::value_type result = {};
+//    for (auto iter = cnt.cbegin(); iter < cnt.cend(); /*iter++*/)
+//    {
+//        if (*iter != 0) {
+//            result.push_back(iter);
+//            ++iter;
+//        }
+//    }
+//
+//    return result;
+////}
+//template <typename Container>
+//Container get_maximum_product_range(const Container& input)
+//{
+//    Container output;
+//
+//    // logic to compute the maximum product range
+//
+//    return output;
+//}
+
+template<typename Container, typename T = typename Container::value_type>
+Container get_maximum_product_range(const Container& input) {
+    Container modified_input(input);
+
+    modified_input.erase(std::remove(modified_input.begin(), modified_input.end(), T(0)), modified_input.end());
+
+    int negative_count = 0;
+    for (const auto& value : modified_input) {
+        if (value < 0) {
+            negative_count++;
+        }
+    }
+
+   auto highest_negative = std::numeric_limits<T>::min();
+    for (const auto& value : modified_input) {
+        if (value < 0 && value > highest_negative) {
+            highest_negative = value;
+        }
+    }
+
+    return modified_input;
+}
 
 void question_2();
 
